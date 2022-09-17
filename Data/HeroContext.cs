3d4ffa0f0a28aca7bd3d9f37ba7ee942c1fc481a
João Paulo Vieira <contato@joaopaulovieira.dev>
2022-09-17 19:34:31 -0300

    Entidades do Contexto

--- Data/HeroContext.cs
+++ Data/HeroContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,13 @@ namespace EFCore.WebAPI.Data
 {
     public class HeroContext : DbContext
     {
+        public DbSet<Hero> Heroes { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
     }
 }
