7ea98eda532472b0cc1cd9d63301ea9487592a3e
João Paulo Vieira <contato@joaopaulovieira.dev>
2022-09-17 20:57:17 -0300

    Script DB e Drop DB

---
+++ obj/Debug/netcoreapp2.2/b4dwwnxt.sql
@@ -0,0 +1,55 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Battles] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(max) NULL,
+    [Descricao] nvarchar(max) NULL,
+    [DataInicio] datetime2 NOT NULL,
+    [DataFim] datetime2 NOT NULL,
+    CONSTRAINT [PK_Battles] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [Heroes] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(max) NULL,
+    [BatalhaId] int NOT NULL,
+    CONSTRAINT [PK_Heroes] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Heroes_Battles_BatalhaId] FOREIGN KEY ([BatalhaId]) REFERENCES [Battles] ([Id]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE TABLE [Weapons] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(max) NULL,
+    [HeroiId] int NOT NULL,
+    CONSTRAINT [PK_Weapons] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Weapons_Heroes_HeroiId] FOREIGN KEY ([HeroiId]) REFERENCES [Heroes] ([Id]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE INDEX [IX_Heroes_BatalhaId] ON [Heroes] ([BatalhaId]);
+
+GO
+
+CREATE INDEX [IX_Weapons_HeroiId] ON [Weapons] ([HeroiId]);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20220917230533_initial', N'2.2.6-servicing-10079');
+
+GO
+
