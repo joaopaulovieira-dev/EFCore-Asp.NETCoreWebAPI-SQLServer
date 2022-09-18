e05f2e23bf78eed0f5eef77a7a957b40628a33ea
João Paulo Vieira <contato@joaopaulovieira.dev>
2022-09-17 22:03:09 -0300

    Scaffold - Do banco para Model

---
+++ EFCore.Legacy/Weapons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCore.Legacy
+{
+    public partial class Weapons
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int HeroiId { get; set; }
+
+        public virtual Heroes Heroi { get; set; }
+    }
+}
